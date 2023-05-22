792f0c4bb80f45e567efb5c73160404d5fafd64a
Hiran <rhirankaushalr@gmail.com>
2023-05-22 13:48:45 +0530

    removed the countdown because of lot of bug gonna add it later

--- src/components/CountDown.js
+++ src/components/CountDown.js
@@ -1,41 +1,46 @@
-import React from "react";
-import { useState,  useRef, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 
-function formatTime (time) {
-    let minutes = Math.floor (time / 60)
-    let seconds = Math.floor (time - minutes * 60)
+function formatTime(time) {
+  let minutes = Math.floor(time / 60);
+  let seconds = Math.floor(time - minutes * 60);
 
-    if (minutes <= 10) minutes = '0' + minutes; 
-    if (seconds <= 10) seconds = '0' + seconds; 
-    return minutes + ':' + seconds
+  if (minutes <= 10) minutes = '0' + minutes; 
+  if (seconds <= 10) seconds = '0' + seconds; 
+  return minutes + ':' + seconds;
 }
 
-function CountDown({ seconds, console, updateCountdown }) {
-    const [countdown, setCountdown] = useState(seconds);
-    const timerId = useRef();
-  
-    useEffect(() => {
-      timerId.current = setInterval(() => {
-        setCountdown((prevCount) => prevCount - 1);
-      }, 100);
-      return () => clearInterval(timerId.current);
-    }, []);
-  
-    useEffect(() => {
-      if (countdown === 0) {
-        clearInterval(timerId.current);
-        console();
-      }
-      // Call the updateCountdown callback to update countdown state in App.js
-      updateCountdown(countdown);
-    }, [countdown, console, updateCountdown]);
-  
-    return (
-      <>
-        <h2>Count Down: {formatTime(countdown)}</h2>
-      </>
-    );
-  }
-  
+function CountDown({ seconds, console, updateCountdown, isRunning }) {
+  const [countdown, setCountdown] = useState(seconds);
+  const timerId = useRef();
+
+  useEffect(() => {
+
+    if (!isRunning) {
+        if (!isRunning) {
+            return
+        }
+    }
+
+    timerId.current = setInterval(() => {
+      setCountdown((prevCount) => prevCount - 1);
+    }, 100);
+    return () => clearInterval(timerId.current);
+  }, []);
+
+  useEffect(() => {
+    if (countdown === 0) {
+      clearInterval(timerId.current);
+      console();
+    }
+    // Call the updateCountdown callback to update countdown state in App.js
+    updateCountdown(countdown);
+  }, [countdown, console, updateCountdown]);
+
+  return (
+    <>
+      <h2>Count Down: {formatTime(countdown)}</h2>
+    </>
+  );
+}
 
 export default CountDown;
